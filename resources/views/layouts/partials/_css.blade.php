3207c74462103fa2124857188664ec0ac38a3f57
ajpadilla <ajpadilla88@gmail.com>
2016-05-03 18:46:36 -0430

    Actualizando blades

--- resources/views/layouts/partials/_css.blade.php
+++ resources/views/layouts/partials/_css.blade.php
@@ -1,4 +1,4 @@
-{{ HTML::link("http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" type="text/css") }}
-{{ HTML::link("//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css") }}
-{{ HTML::style("assets/css/bootstrap-3.3.6-dist/bootstrap.min.css") }}
-{{ HTML::style("assets/css/custom/styles.css") }}
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/bootstrap-3.3.6-dist/css/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/custom/styles.css') }}">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
--- resources/views/layouts/partials/_js.blade.php
+++ resources/views/layouts/partials/_js.blade.php
@@ -1,2 +1,2 @@
-{{ HTML::script("assets/js/jquery-2.2.3.min.js") }}
-{{ HTML::script("assets/css/bootstrap-3.3.6-dist/js/bootstrap.min.js") }}
+<script type="text/javascript"  src="{{ URL::asset('assets/js/jquery-2.2.3.min.js') }}"></script>
+<script type="text/javascript"  src="{{ URL::asset('assets/css/bootstrap-3.3.6-dist/js/bootstrap.min.js') }}"></script>
