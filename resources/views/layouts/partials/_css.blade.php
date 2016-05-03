fa13e117adc9d35a82d8052131a0a79fe07b20d7
ajpadilla <ajpadilla88@gmail.com>
2016-05-03 17:47:02 -0430

    Actualizando blade

--- resources/views/layouts/partials/_css.blade.php
+++ resources/views/layouts/partials/_css.blade.php
@@ -1,4 +1,4 @@
 {{ HTML::link("http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" type="text/css") }}
-{{ HTML::style("assets/css/bootstrap-3.3.6-dist/bootstrap.min.css") }}
 {{ HTML::link("//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css") }}
-{{ HTML::style("css/custom/styles.css") }}
+{{ HTML::style("assets/css/bootstrap-3.3.6-dist/bootstrap.min.css") }}
+{{ HTML::style("assets/css/custom/styles.css") }}
