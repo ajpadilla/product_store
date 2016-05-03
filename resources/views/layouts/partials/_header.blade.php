8a1792f9f7732e1b5702a0a3e925c8b135d96177
ajpadilla <ajpadilla88@gmail.com>
2016-05-03 18:47:43 -0430

    Agregando blade con contenido del header

---
+++ resources/views/layouts/partials/_header.blade.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">Brand</a>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Dashboard</a></li>
+				<li><a href="#">Settings</a></li>
+				<li><a href="#">Profile</a></li>
+				<li><a href="#">Help</a></li>
+			</ul>
+			<!--<div class="col-sm-6 col-md-6 col-lg-6 col-lg-push-2">
+				<form class="navbar-form" style="width:80%" role="search">
+					<div style="display:table;" class="input-group">
+						<input type="text" autofocus="autofocus" autocomplete="off" placeholder="Search Here" name="search" style="" class="form-control">
+						<span style="width: 1%;" class="input-group-addon">
+							<span class="glyphicon glyphicon-search"></span>
+						</span>
+					</div>
+				</form>
+			</div>-->
+			<form id="adv-search">
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="Buscar">
+					<div class="input-group-btn">
+						<div class="btn-group" role="group">
+							<button type="submit" class="btn btn-primary">
+								<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+							</button>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</nav>
