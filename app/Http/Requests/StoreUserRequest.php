7ef2622ebd7f5cd969c305c28bd561f9635545b4
ajpadilla <ajpadilla88@gmail.com>
2016-05-04 16:01:48 -0430

    Agreagando validación confirm al password

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -28,8 +28,8 @@ class StoreUserRequest extends Request
             'last_name' => 'required|min:3|max:60',
             'email' => 'required|email|unique:users,email',
             'username' => 'required|unique:users,username|max:50',
-            'password' => 'required|min:6|max:60',
-            'password_confirmation' => 'required|min:6'
+            'password' => 'required|min:6|max:60|confirmed',
+            'password_confirmation' => 'required|min:6|max:60',
             'photo' => 'mimes:jpeg,jpg,png,gif|max:10000'
         ];
     }
