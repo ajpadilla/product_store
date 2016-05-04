1d19dadad4247618ac10cc3fe9c6fe6dc9063bac
ajpadilla <ajpadilla88@gmail.com>
2016-05-04 15:12:33 -0430

    Agregando reglas de validación para almacenar nuevo usuario

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class StoreUserRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|min:3|max:60',
+            'last_name' => 'required|min:3|max:60',
+            'email' => 'required|email|unique:users,email',
+            'username' => 'required|unique:users,username|max:50',
+            'password' => 'required|min:6|max:60',
+            'password_confirmation' => 'required|min:6'
+            'photo' => 'mimes:jpeg,jpg,png,gif|max:10000'
+        ];
+    }
+}
