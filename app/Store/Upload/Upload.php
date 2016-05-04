8c1e8a5c203330a6dea02a966bc8a5a671f4819e
ajpadilla <ajpadilla88@gmail.com>
2016-05-04 18:57:20 -0430

    Creando constructor de la clase

--- app/Store/Upload/Upload.php
+++ app/Store/Upload/Upload.php
@@ -1,12 +1,35 @@
 <?php  
 	namespace App\Store\Upload;
-	
+
+	use DateTime;
+	use Intervention\Image\Facades\Image;
+	use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 	class Upload
 	{
+
+		private $file;
+		private $fileName;
+		private $fileNameInDir;
+		private $thumbnailFileNameInDir;
+		private $fileExtension;
+		private $size;
+		private $mimeType;
+		private $uploadPath;
+		private $publicFilePath;
+		private $thumbnailWidth;
+		private $thumbnailHeight;
 		
-		function __construct(argument)
+		function __construct(UploadedFile $file, $thumbnailWidth = 74, $thumbnailHeight = 103, $publicFilePath='')
 		{
-			# code...
+			$this->file = $file;
+			$this->fileName = substr($file->getClientOriginalName(), 0, -4);
+			$this->fileExtension = 'jpg';
+			$this->size = $file->getSize();
+			$this->mimeType = $file->getMimeType();
+			$this->thumbnailWidth = $thumbnailWidth;
+			$this->thumbnailHeight = $thumbnailHeight;
+			$this->publicFilePath = $publicFilePath;
 		}
 	}
 ?>
