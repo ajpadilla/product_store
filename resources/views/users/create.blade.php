5d51bb5e20a135048e5d289653db62795c326f2f
ajpadilla <ajpadilla88@gmail.com>
2016-05-03 22:32:27 -0430

    Actualizando formulariop

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,11 +3,14 @@
 @section('title', 'Add new user')
 
 @section('content')
+	<h1 class="page-header">Add new user</h1>
 	{!! Form::open(['route' => 'user.store', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'POST', 'files' => true, 'accept-charset' => 'UTF-8', 'enctype' => 'multipart/form-data']) !!}
 		{!! csrf_field() !!}
 		<div class="form-group">
-			{{ Form::label('first_name', 'First Name') }}
-			{{ Form::text('first_name','First Name',['class' => 'form-control']) }}
+			{!! Form::label('first_name', 'First Name', ['class' => 'col-sm-2 control-label']) !!}
+			<div class="col-sm-4">
+				{!! Form::text('first_name','First Name', ['class' => 'form-control']) !!}
+			</div>
 		</div>
 	{!! Form::close() !!}
 @endsection
