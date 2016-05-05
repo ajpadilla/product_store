44f3330b20802785524c08871720aa04083b9bf8
ajpadilla <ajpadilla88@gmail.com>
2016-05-04 23:30:30 -0430

    Agregando metodo register, para almacenar datos de fotos para usuarios

--- app/Store/Upload/UserPhotoRepository.php
+++ app/Store/Upload/UserPhotoRepository.php
@@ -2,8 +2,9 @@
  namespace App\Store\Upload;
 
  use App\Store\Base\BaseRepository;
-  use App\Store\Upload\Upload;
+ use App\Store\Upload\Upload;
  use App\Store\Upload\UserPhoto;
+ use Symfony\Component\HttpFoundation\File\UploadedFile;
 /**
 * 
 */
@@ -15,9 +16,20 @@ class UserPhotoRepository extends BaseRepository
 		  $this->setModel(new UserPhoto);
 	}
 
-	public function FunctionName($value='')
+	public function register(UploadedFile $file, $user_id)
 	{
-		# code...
+		$this->upload = new Upload($file, 74, 103, public_path().'\storage\users\\');
+		$this->upload->process();
+		$data = array('complete_path' => $this->upload->getCompletePublicFilePath(),
+				'complete_thumbnail_path'=> $this->upload->getCompleteThumbnailPublicFilePath(),
+				'filename' => $this->upload->getFileName(),
+				'path' => $this->upload->getUploadPath(),
+				'extension' => $this->upload->getFileExtension(),
+				'size' => $this->upload->getSize(),
+				'mimetype' => $this->upload->getMimeType(),
+				'user_id' => $user_id
+			);
+		parent::create($data);
 	}
 }
 
