cc52aa9216567b345f9b75ea2ad5c1f1b22c271d
ajpadilla <ajpadilla88@gmail.com>
2016-05-04 22:35:27 -0430

    Agregando metodo user

--- app/Store/Upload/UserPhoto.php
+++ app/Store/Upload/UserPhoto.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class UserPhoto extends Model
 {
     protected $fillable = ['first_name','last_name','username', 'email', 'password'];
+
+    public function user()
+    {
+   		$this->belongsTo('App\Store\User\User');
+    }
 }
