886f1832a774765794ccd60974e58903ae1ee91d
ajpadilla <ajpadilla88@gmail.com>
2016-05-16 15:05:37 -0430

    Agregando migración para tabla fotos de productos

---
+++ database/migrations/2016_05_16_193432_create_product_photos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductPhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_photos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('filename');
+            $table->string('path');
+            $table->string('complete_path');
+            $table->string('complete_thumbnail_path');
+            $table->integer('size');
+            $table->string('extension', 3);
+            $table->string('mimetype', 32);
+            $table->integer('product_id')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('product_photos');
+    }
+}
