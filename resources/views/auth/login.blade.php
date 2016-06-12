edab5bf796eaa58c4fcc1b3d685edfd7185167ba
ajpadilla <ajpadilla88@gmail.com>
2016-06-12 14:34:14 -0430

    Modificando uso de etiquetas html convenciona por etiquetas laravelcollective

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,34 +3,33 @@
 @section('title', 'Login')
 
 @section('main-content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-4 col-md-offset-4">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h3 class="panel-title">Login via site</h3>
-				</div>
-				<div class="panel-body">
-					<form accept-charset="UTF-8" role="form">
-						<fieldset>
-							<div class="form-group">
-								<input class="form-control" placeholder="yourmail@example.com" name="email" type="text">
-							</div>
-							<div class="form-group">
-								<input class="form-control" placeholder="Password" name="password" type="password" value="">
-							</div>
-							<div class="checkbox">
-								<label>
-									<input name="remember" type="checkbox" value="Remember Me"> Remember Me
-								</label>
-							</div>
-							<input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
-						</fieldset>
-					</form>
-					<hr/>
-					<center><h4>OR</h4></center>
-					<input class="btn btn-lg btn-facebook btn-block" type="submit" value="Login via facebook">
-				</div>
+<div class="row">
+	<div class="col-md-4 col-md-offset-4">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Login via site</h3>
+			</div>
+			<div class="panel-body">
+				{!! Form::open(['url' => 'auth/login', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'POST', 'accept-charset' => 'UTF-8']) !!}
+					{!! csrf_field() !!}
+					<fieldset>
+						<div class="form-group">
+							{!! Form::email('email','', ['class' => 'form-control', 'placeholder' => 'Email']) !!}
+						</div>
+						<div class="form-group">
+							{!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}
+						</div>
+						<div class="checkbox">
+							<label>
+								{!! Form::checkbox('remember', 'Remember Me') !!} Remember Me
+							</label>
+						</div>
+						{!! Form::submit('Login',['class' =>'btn btn-lg btn-success btn-block']) !!}
+					</fieldset>
+				{!! Form::close() !!}
+				<hr/>
+				<center><h4>OR</h4></center>
+				{!! Form::submit('Login via facebook',['class' =>'btn btn-lg btn-facebook btn-block']) !!}
 			</div>
 		</div>
 	</div>
