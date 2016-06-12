428ebfeae870204832a2d52fe6a35d5757c5c526
ajpadilla <ajpadilla88@gmail.com>
2016-06-12 13:49:16 -0430

    Agregando nuevo blade para contenido del dashboar para usuarios

---
+++ resources/views/layouts/content.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('main-content')
+@include("layouts.partials._header")
+<div class="row">
+	<div class="col-sm-3 col-md-2">
+		@include('layouts.partials._sidebar')
+	</div>
+	<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+		<div id="content">
+			@yield('content')
+		</div>
+	</div>
+</div>
+@endsection
