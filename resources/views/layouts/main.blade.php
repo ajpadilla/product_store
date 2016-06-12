09295300363d1650a467a1dfb32ac983d6c183a6
ajpadilla <ajpadilla88@gmail.com>
2016-06-12 13:46:09 -0430

    Modificando estreuctura del blade principal

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,18 +7,8 @@
 	@include('layouts.partials._css')
 </head>
 <body>
-	@include("layouts.partials._header")
 	<div class="container-fluid">
-		<div class="row">
-			<div class="col-sm-3 col-md-2">
-				@include('layouts.partials._sidebar')
-			</div>
-			<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-				<div id="content">
-					@yield('content')
-				</div>
-			</div>
-		</div>
+		@yield('main-content')
 	</div>
 	@include("layouts.partials._js")
 </body>
