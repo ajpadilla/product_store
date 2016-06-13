25dbf39aa69be4520d8f6cf653965274651b7c0b
ajpadilla <ajpadilla88@gmail.com>
2016-06-13 10:42:21 -0430

    Agregando sección para mostrar errores al loguear usuarios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,8 +3,10 @@
 @section('title', 'Login')
 
 @section('main-content')
+
 <div class="row">
 	<div class="col-md-4 col-md-offset-4">
+		@include('layouts.partials._errors')
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<h3 class="panel-title">Login via site</h3>
