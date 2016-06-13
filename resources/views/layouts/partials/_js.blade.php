ed32ad4b89109e4734f33d0a23153b3fb39930f9
ajpadilla <ajpadilla88@gmail.com>
2016-06-13 18:21:04 -0430

    Agregando archivo custom

--- resources/views/layouts/partials/_js.blade.php
+++ resources/views/layouts/partials/_js.blade.php
@@ -1,2 +1,3 @@
 <script type="text/javascript"  src="{{ URL::asset('assets/js/jquery-2.2.3.min.js') }}"></script>
 <script type="text/javascript"  src="{{ URL::asset('assets/css/bootstrap-3.3.6-dist/js/bootstrap.min.js') }}"></script>
+{!! Html::script('assets/js/custom.js') !!}
