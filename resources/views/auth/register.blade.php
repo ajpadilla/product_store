5586f6ae0f0b8c7e629facdf86ca8b16c46fea31
ajpadilla <ajpadilla88@gmail.com>
2016-06-13 13:18:21 -0430

    Agregando formulario para registrar usuarios al sistema

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.main')
+
+@section('title', 'Login')
+
+@section('main-content')
+<div class="row">
+	<div class="col-md-4 col-md-offset-4">
+		@include('layouts.partials._errors')
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Login via site</h3>
+			</div>
+			<div class="panel-body">
+				{!! Form::open(['url' => 'auth/login', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'POST', 'accept-charset' => 'UTF-8']) !!}
+				{!! csrf_field() !!}
+				<fieldset>
+					<div class="form-group">
+						{!! Form::email('email','', ['class' => 'form-control', 'placeholder' => 'Email']) !!}
+					</div>
+					<div class="form-group">
+						{!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}
+					</div>
+					<div class="checkbox">
+						<label>
+							{!! Form::checkbox('remember', 'Remember Me') !!} Remember Me
+						</label>
+					</div>
+					{!! Form::submit('Login',['class' =>'btn btn-lg btn-success btn-block']) !!}
+				</fieldset>
+				{!! Form::close() !!}
+				<hr/>
+				<center><h4>OR</h4></center>
+				{!! Form::submit('Login via facebook',['class' =>'btn btn-lg btn-facebook btn-block']) !!}
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
