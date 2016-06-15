796af7067f21d0d74ba1bcb9d3751e29ced7fe47
ajpadilla <ajpadilla88@gmail.com>
2016-06-15 18:01:03 -0400

    Agragando filtro para verificar si el usuario esta activo al iniciar sesion

---
+++ app/Http/Middleware/ActiveUserMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Contracts\Auth\Guard;
+use Closure;
+
+class ActiveUserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function handle($request, Closure $next)
+    {
+         if($this->auth->check() && $this->auth->user()->active !== 1){
+            $this->auth->logout();
+            return redirect('auth/login')->withErrors('sorry, this user account is deactivated');
+        }
+        return $next($request);
+    }
+}
