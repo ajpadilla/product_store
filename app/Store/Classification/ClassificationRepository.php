c0910cbeb6d98e5ea4a4e41a0beaabae6feea6a9
ajpadilla <ajpadilla88@gmail.com>
2016-06-17 17:39:13 -0400

    Agregando metodo table

--- app/Store/Classification/ClassificationRepository.php
+++ app/Store/Classification/ClassificationRepository.php
@@ -3,6 +3,7 @@
 
 	use App\Store\Base\BaseRepository;
 	use App\Store\Classification\Classification;
+	use Yajra\Datatables\Datatables;
 
 	/**
 	* 
@@ -14,5 +15,20 @@
 		{
 			$this->setModel(new Classification);
 		}
+
+
+		public function table()
+		{
+			$classifications = $this->getModel()->select(['id','name']);
+			return Datatables::of($classifications)
+			->addColumn('action', function($classification){
+				return '<div class="col-md-6 text-center"> 
+							<a href="#edit-classification-'.$classification->id.'" class="btn btn-primary"><i class="glyphicon glyphicon-edit"></i>Edit</a>
+							 <a href="#delete-classification-'.$classification->id.'" class="btn btn-danger"><i class="glyphicon glyphicon-remove"></i>Delete</a>
+						</div>';
+			})
+			->make(true);
+		}
+
 	}
 ?>
