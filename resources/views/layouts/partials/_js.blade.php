572b84659ba5d159f337cf0c48b6902171de10dd
ajpadilla <ajpadilla88@gmail.com>
2016-06-17 17:40:35 -0400

    Actualizando blad

--- resources/views/layouts/partials/_js.blade.php
+++ resources/views/layouts/partials/_js.blade.php
@@ -1,3 +1,6 @@
 <script type="text/javascript"  src="{{ URL::asset('assets/js/jquery-2.2.3.min.js') }}"></script>
+<script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript"  src="{{ URL::asset('assets/css/bootstrap-3.3.6-dist/js/bootstrap.min.js') }}"></script>
+@yield('scripts')
 {!! Html::script('assets/js/custom.js') !!}
+
