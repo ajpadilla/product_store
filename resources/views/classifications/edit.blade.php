61acd814c9b6a667212075543ee6a3a0a3c63582
ajpadilla <ajpadilla88@gmail.com>
2016-06-17 17:38:11 -0400

    Agregand formulario para editar clasificación

---
+++ resources/views/classifications/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.content')
+
+@section('title', 'Edit new classification')
+
+@section('content')
+	<h1 class="page-header">Edit classification for products</h1>
+	@include('layouts.partials._errors')
+	@include('layouts.partials._messages')
+	{!! Form::open(['route' => 'classification.store', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'POST']) !!}
+		{!! csrf_field() !!}
+		<div class="form-group">
+			{!! Form::label('name', '', ['class' => 'col-sm-2 control-label']) !!}
+			<div class="col-sm-4">
+				{!! Form::text('name','', ['class' => 'form-control', 'placeholder' => 'Name']) !!}
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-6 col-sm-offset-2">
+				{!! Form::submit('Add',['class' =>'btn btn-primary']) !!}
+			</div>
+		</div>
+	{!! Form::close() !!}
+@endsection
