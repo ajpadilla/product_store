209609191eea3b02c4cf7b810f6cc6257aed22de
ajpadilla <ajpadilla88@gmail.com>
2016-06-17 10:53:30 -0400

    Agregando objeto ClassificationRepository

--- app/Http/Controllers/ClassificationController.php
+++ app/Http/Controllers/ClassificationController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreClassificationRequest;
+    use App\Store\Classification\Classification\ClassificationRepository;
 
 class ClassificationController extends Controller
 {
@@ -38,7 +39,7 @@ class ClassificationController extends Controller
      */
     public function store(StoreClassificationRequest $request)
     {
-        //
+        
     }
 
     /**
