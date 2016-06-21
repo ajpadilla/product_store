28619c3bcb4eeabce90bc5510759bf7cffbcfb93
ajpadilla <ajpadilla88@gmail.com>
2016-06-21 16:45:45 -0400

    Agregando reglas para validar campos del formulario add new products

---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class StoreProductRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:50', 
+            'description' => 'required|max:256', 
+            'quantity' => 'required|integer', 
+            'price' => 'required|numeric', 
+            'active' => 'required|boolean', 
+            'mark' => 'required', 
+            'classification_id' => 'required|exists:classifications,id', 
+            //'user_id' => 'required|exists:users,id'
+        ];
+    }
+}
