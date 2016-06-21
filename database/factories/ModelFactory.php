d16db89aa7c76a24cfd4799d39cf967d68e091e1
ajpadilla <ajpadilla88@gmail.com>
2016-06-21 16:04:02 -0400

    Agregando fakers para modelo products

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -30,3 +30,17 @@ $factory->define(App\Store\User\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+$factory->define(App\Store\Product\Product::class, function (Faker\Generator $faker) {
+    return [
+       'name' => $faker->word, 
+       'description' => $faker->text($maxNbChars = 50), 
+       'quantity' => $faker->numberBetween($min = 1, $max = 100), 
+       'price' => $faker->randomFloat($nbMaxDecimals = 6, $min = 1, $max = 1000), 
+       'active' => rand(0, 1), 
+       'mark' => $faker->word, 
+       'classification_id' => rand(1, 10), 
+       'user_id' => rand(1, 10)
+    ];
+});
