66b54c2628ef2f1d82c8fe0441b8d2d9cf7a9815
ajpadilla <ajpadilla88@gmail.com>
2016-06-23 20:20:28 -0400

    Agregando archivos .js para plugin dropzone

--- resources/views/layouts/partials/_js.blade.php
+++ resources/views/layouts/partials/_js.blade.php
@@ -1,7 +1,8 @@
 <script type="text/javascript"  src="{{ URL::asset('assets/js/jquery-2.2.3.min.js') }}"></script>
 <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript"  src="{{ URL::asset('assets/css/bootstrap-3.3.6-dist/js/bootstrap.min.js') }}"></script>
-@yield('scripts')
+{!! Html::script('assets/js/dropzone-4.3.0/dist/dropzone.js') !!}
 {!! Html::script('assets/js/bootbox.min.js') !!}
 {!! Html::script('assets/js/custom.js') !!}
+@yield('scripts')
 
