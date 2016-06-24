f0f29e51b0af2fa35e2ba0fa0729ef0f0f1d6061
ajpadilla <ajpadilla88@gmail.com>
2016-06-23 20:20:08 -0400

    Agregando archivos .css para estilos de plugin dropzone

--- resources/views/layouts/partials/_css.blade.php
+++ resources/views/layouts/partials/_css.blade.php
@@ -3,3 +3,4 @@
 <link rel="stylesheet" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
 <link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/custom/styles.css') }}">
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+{!! Html::style('assets/js/dropzone-4.3.0/dist/dropzone.css') !!}
