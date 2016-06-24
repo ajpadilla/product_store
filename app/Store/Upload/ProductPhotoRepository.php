61df9099cf5127c13f234344c5270c18f07e6ce3
ajpadilla <ajpadilla88@gmail.com>
2016-06-24 11:00:45 -0400

    Agregando repositorio para crear y manipulametodos del modelo ProductPhoto

---
+++ app/Store/Upload/ProductPhotoRepository.php
@@ -0,0 +1,37 @@
+<?php  
+ namespace App\Store\Upload;
+
+ use App\Store\Base\BaseRepository;
+ use App\Store\Upload\Upload;
+ use App\Store\Upload\ProductPhoto;
+ use Symfony\Component\HttpFoundation\File\UploadedFile;
+/**
+* 
+*/
+class ProductPhotoRepository extends BaseRepository
+{
+	protected $upload;
+	function __construct()
+	{
+		  $this->setModel(new ProductPhoto);
+	}
+
+	public function register(UploadedFile $file, $product_id, $user_id)
+	{
+		$this->upload = new Upload($file, 74, 103, public_path().'\storage\products\\');
+		$this->upload->process();
+		$data = array('complete_path' => $this->upload->getCompletePublicFilePath(),
+				'complete_thumbnail_path'=> $this->upload->getCompleteThumbnailPublicFilePath(),
+				'filename' => $this->upload->getFileName(),
+				'path' => $this->upload->getUploadPath(),
+				'extension' => $this->upload->getFileExtension(),
+				'size' => $this->upload->getSize(),
+				'mimetype' => $this->upload->getMimeType(),
+				'product_id' => $product_id,
+				'user_id' => $user_id
+			);
+		parent::create($data);
+	}
+}
+
+?>
