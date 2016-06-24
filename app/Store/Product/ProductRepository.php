d596d4da9ac3a050a003d04eeab2b098f786e25a
ajpadilla <ajpadilla88@gmail.com>
2016-06-24 11:53:49 -0400

    Agregando objetoi use Yajra\Datatables\Datatables;

--- app/Store/Product/ProductRepository.php
+++ app/Store/Product/ProductRepository.php
@@ -3,7 +3,7 @@
 
 	use App\Store\Base\BaseRepository;
 	use App\Store\Product\Product;
-
+	use Yajra\Datatables\Datatables;
 	/**
 	* 
 	*/
@@ -14,6 +14,19 @@
 		{
 			$this->setModel(new Product);
 		}
+
+		public function table()
+		{
+			$products = $this->getModel()->select(['id','name','description','quantity', 'price', 'mark', 'classification_id']);
+			return Datatables::of($products)
+			->addColumn('action', function($product){
+				return "<a  class='btn btn-primary' href='" . route('classification.edit', $product->id) . "'><i class='glyphicon glyphicon-edit'></i>Edit</a>
+					<a  class='btn btn-danger delete-products' href='" . route('classification.delete', $product->id) . "' id='delete_products_".$product->id."'><i class='glyphicon glyphicon-remove'></i>Delete</a>
+				";
+			})
+			->make(true);
+		}
+
 	}
 
 ?>
