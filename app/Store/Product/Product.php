ded7843da8b5cf809d4b262e8bd15b9fc7bd775e
ajpadilla <ajpadilla88@gmail.com>
2016-06-24 12:12:12 -0400

    Agregando metodo classification

--- app/Store/Product/Product.php
+++ app/Store/Product/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
 	protected $fillable = ['name', 'description', 'quantity', 'price', 'active', 'mark', 'classification_id', 'user_id'];
+
+	public function classification()
+	{
+		 return $this->belongsTo('App\Store\Classification\Classification');
+	}
 }
