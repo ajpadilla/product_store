c1cdda258cf9a519139fd8379b97c0ea50371450
ajpadilla <ajpadilla88@gmail.com>
2016-07-01 10:06:53 -0400

    Agregando middlerware para verificar a usuario administrador activo del sistema

---
+++ app/Http/Middleware/ConfirmAdmin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Contracts\Auth\Guard;
+use Closure;
+
+class ConfirmAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function handle($request, Closure $next)
+    {
+         if($this->auth->check() && $this->auth->user()->active && $this->auth->user()->role != 'admin'){
+            $this->auth->logout();
+            return redirect('auth/login')->withErrors('sorry, this account is only for admin users');
+        }
+        return $next($request);
+    }
+}
