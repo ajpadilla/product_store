c7356d25ed9ae62da95ec29b2c9dcab07c263111
ajpadilla <ajpadilla88@gmail.com>
2016-07-05 21:20:26 -0400

    Agregando tabla pivote para agregar productos a carrito de compras de usuarios

---
+++ database/migrations/2016_07_06_011532_create_cart_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_product', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('cart_id')->unsigned()->index();
+            $table->integer('product_id')->unsigned()->index();
+            $table->smallInteger('quantity')->default(1);
+            $table->unique(array('cart_id', 'product_id'), 'cart_product_unique');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cart_product');
+    }
+}
