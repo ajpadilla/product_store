7010c38466c18d7818c0d53bcb7a4c07f95b3e3d
ajpadilla <ajpadilla88@gmail.com>
2016-07-05 23:59:55 -0400

    Codificando metodo create para insetar nuevo elemento en la tabla

--- app/Store/Cart/CartRepository.php
+++ app/Store/Cart/CartRepository.php
@@ -8,19 +8,29 @@
 	/**
 	* 
 	*/
-	class ClassName extends BaseRepository
+	class CartRepository extends BaseRepository
 	{
 		
 		function __construct()
 		{
 			$this->setModel(new Cart);
 		}
-	}
 
-	public function getActiveCartForUser(User $user)
-	{
-		return $this->getModel()->whereUserId($user->id)
-				->whereActive(TRUE)->orderBy('create_at', 'DESC')
-				->first();
+		public function create($data = array())
+		{
+			$cart = $this->model->create($data);
+			$user = \Auth::user();
+			$cart->user()->associate($user);
+			$cart->save();
+			return $cart;
+		}
+
+		public function getActiveCartForUser(User $user)
+		{
+			return $this->getModel()->whereUserId($user->id)
+					->whereActive(TRUE)->orderBy('created_at', 'DESC')
+					->first();
+		}
+
 	}
 ?>
