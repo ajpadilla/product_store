4e114ecbf6ca544f93d95cc6948912dde17f7345
ajpadilla <ajpadilla88@gmail.com>
2016-07-05 22:41:57 -0400

    Agregando datos del admin del sistema de prueba

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+    	\DB::table('users')->insert([
+    		'first_name' => 'Alvaro',
+	        'last_name' => 'Padilla',
+	        'username' => 'ajpadilla88',
+	        'address' => 'Las Delicias',
+	        'post_code' => '5001',
+	        'country_id' => rand(1, 246),
+	        'phone' => '0416-1342017' ,
+	        'active' => '1',
+	        'role' => 'admin',
+	        'email' => 'ajpadilla88@gmail.com',
+	        'password' => '123456',
+	        'remember_token' => str_random(10),
+    	]);
         factory(User::class, 10)->create();
     }
 }
