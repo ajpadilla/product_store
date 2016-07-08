3f1ed62570c09c16aedff07bd19ce5c3fcb35d54
ajpadilla <ajpadilla88@gmail.com>
2016-07-08 17:51:33 -0400

    Agragando objecto productRepository

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,13 +7,17 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Store\Cart\CartRepository;
+use App\Store\Product\ProductRepository;
 
 class CartController extends Controller
 {
+    protected $cartRepository;
+    protected $productRepository;
 
-    public function __construct(CartRepository $cartRepository)
+    public function __construct(CartRepository $cartRepository, ProductRepository $productRepository)
     {
         $this->cartRepository = $cartRepository;
+        $this->productRepository = $productRepository;
     }
 
     /**
@@ -42,9 +46,16 @@ class CartController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $productId, $quantity = 1)
     {
-        //
+        //if ($request->ajax()) 
+        //{
+            if (!$this->cartRepository->getActiveCartForUser(\Auth::user())) 
+            {
+                $input['active'] = TRUE;
+                $this->cartRepository->create($input);
+            }
+       // }
     }
 
     /**
