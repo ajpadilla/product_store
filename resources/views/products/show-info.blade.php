f99fbdb65ce60fe7cbbebf0d1fe9842663ed7891
ajpadilla <ajpadilla88@gmail.com>
2016-07-11 11:49:28 -0400

    Agregando información para cada producto

--- resources/views/products/show-info.blade.php
+++ resources/views/products/show-info.blade.php
@@ -9,31 +9,40 @@
 			<div class="col-md-12">
 				<div class="product col-md-6 service-image-left">
 					<center>
-						<img id="item-display" src="http://www.corsair.com/Media/catalog/product/g/s/gs600_psu_sideview_blue_2.png" alt=""></img>
+						@if ($product->hasPhotos())
+							<img id="item-display" src="/{{ $product->first_photo->complete_path }}" alt=""></img>
+						@endif
 					</center>
 				</div>
 				<div class="container service1-items col-sm-2 col-md-2 pull-left">
 					<center>
-						<a id="item-1" class="service1-item">
-							<img src="http://www.corsair.com/Media/catalog/product/g/s/gs600_psu_sideview_blue_2.png" alt=""></img>
-						</a>
-						<a id="item-2" class="service1-item">
-							<img src="http://www.corsair.com/Media/catalog/product/g/s/gs600_psu_sideview_blue_2.png" alt=""></img>
-						</a>
-						<a id="item-3" class="service1-item">
-							<img src="http://www.corsair.com/Media/catalog/product/g/s/gs600_psu_sideview_blue_2.png" alt=""></img>
-						</a>
+						@if ($product->hasPhotos())
+							<?php $countPhotos = 0;?>
+							@foreach ($product->photos as $photo)
+								@if ($countPhotos >= 1 && $countPhotos <= 3)
+									<a id="item-1" class="service1-item">
+										<img src="/{{ $photo->complete_thumbnail_path }}" alt=""></img>
+									</a>
+								@endif
+								<?php $countPhotos++?>
+							@endforeach
+						@endif
 					</center>
 				</div>
 			</div>
-
 			<div class="col-md-7">
-				<div class="product-title">Corsair GS600 600 Watt PSU</div>
-				<div class="product-desc">The Corsair Gaming Series GS600 is the ideal price/performance choice for mid-spec gaming PC</div>
-				<div class="product-rating"><i class="fa fa-star gold"></i> <i class="fa fa-star gold"></i> <i class="fa fa-star gold"></i> <i class="fa fa-star gold"></i> <i class="fa fa-star-o"></i> </div>
+				<div class="product-title">{{ $product->name }}</div>
+				<!--<div class="product-desc"></div>
+				<div class="product-rating">
+					<i class="fa fa-star gold"></i> 
+					<i class="fa fa-star gold"></i> 
+					<i class="fa fa-star gold"></i> 
+					<i class="fa fa-star gold"></i> 
+					<i class="fa fa-star-o"></i> 
+				</div>-->
 				<hr>
-				<div class="product-price">$ 1234.00</div>
-				<div class="product-stock">In Stock</div>
+				<div class="product-price">{{ $product->price }}</div>
+				<div class="product-stock">{{ $product->quantity }}</div>
 				<hr>
 				<div class="btn-group cart">
 					<button type="button" class="add_cart btn btn-success">
@@ -52,22 +61,22 @@
 		<div class="col-md-12 product-info">
 			<ul id="myTab" class="nav nav-tabs nav_tabs">
 				<li class="active"><a href="#service-one" data-toggle="tab">DESCRIPTION</a></li>
-				<li><a href="#service-two" data-toggle="tab">PRODUCT INFO</a></li>
-				<li><a href="#service-three" data-toggle="tab">REVIEWS</a></li>
+				<li><a href="#service-two" data-toggle="tab">PRODUCT MARK</a></li>
+				<li><a href="#service-three" data-toggle="tab">CLASSIFICATION</a></li>
 			</ul>
 			<div id="myTabContent" class="tab-content">
 				<div class="tab-pane fade in active" id="service-one">
 					<section class="container product-info">
-
+						{{ $product->description }}
 					</section>
 				</div>
 				<div class="tab-pane fade" id="service-two">
 					<section class="container">
-
+						{{ $product->mark }}
 					</section>
 				</div>
 				<div class="tab-pane fade" id="service-three">
-
+						{{ $product->classification->name }}
 				</div>
 			</div>
 			<hr>
