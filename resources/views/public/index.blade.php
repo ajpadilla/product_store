7046f5ef4f427f1e0a1c2877f795f7de81a27994
ajpadilla <ajpadilla88@gmail.com>
2016-07-18 10:02:46 -0400

    Agregando sección para mostrar mensajes con alertas en caso de fallas

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -4,6 +4,7 @@
 @include("layouts.partials._header")
 <div class="row">
 	@include('layouts.partials._errors')
+	@include('layouts.partials._messages')
 	<h1>Pagina Principal</h1>
 </div>
 @endsection
