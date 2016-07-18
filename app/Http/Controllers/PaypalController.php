ecdd5efeff0d90f1b21ed36f1e05d7c868433ce8
ajpadilla <ajpadilla88@gmail.com>
2016-07-18 09:35:00 -0400

    Creando metodo postPayment, para enviar datos de compras a paypal

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -119,6 +119,77 @@ class PaypalController extends Controller
 
         $items = array();
         $subtotal = 0;
-        $cart = 
+        $cart = $this->cartRepository->getActiveCartForUser(\Author::user());
+        $currency = 'USD';
+
+        foreach ($cart->products as $product) 
+        {
+            $item = new Item();
+            $item->setName($product->name)
+            ->setCurrency($currency)
+            ->setDescription($product->description)
+            ->setQuantity($product->quantity)
+            ->setPrice($product->price);
+
+            $items[] = $item;
+            $subtotal += $product->quantity * $product->price;
+         }
+
+         $item_list = new ItemList();
+         $item_list->setItems($items);
+
+        $details = new Details();
+        $details->setSubtotal($subtotal)
+        ->setShipping(100);
+
+        $total = $subtotal + 100;
+
+        $amount = new Amount();
+        $amount->setCurrency($currency)
+                ->setTota($total)
+                ->setDetails($details);
+
+        $transaction = new Transaction();
+        $transaction->setAmount($amount)
+                    ->setItemList($item_list)
+                    ->setDescription('Trial order in my laravel App Store');
+
+        $redirect_urls = new RedirectUrls();
+        $redirect_urls->setReturnUrl(\URL::route('payment.status'))
+                    ->setCancelUrl(\URL::route('payment.status'));
+
+        $payment = new Payment();
+        $payment->setIntent('Sale')
+                ->setPayer($payer)
+                ->setRedirectUrls($redirect_urls)
+                ->setTransactions(array($transaction));
+
+        try {
+            $payment->create($this->_api_context);
+        } catch (\PayPal\Exception\PPConnectionException $ex) {
+            if (\Config::get('app.debug')) {
+                echo "Exception: " . $ex->getMessage() . PHP_EOL;
+                $err_data = json_decode($ex->getData(), true);
+                exit;
+            } else {
+                die('Ups! Something went wrong');
+            }
+        }
+
+        foreach($payment->getLinks() as $link) {
+            if($link->getRel() == 'approval_url') {
+                $redirect_url = $link->getHref();
+                break;
+            }
+        }
+
+        // add payment ID to session
+        \Session::put('paypal_payment_id', $payment->getId());
+        if(isset($redirect_url)) {
+            // redirect to paypal
+            return \Redirect::away($redirect_url);
+        }
+        return \Redirect::route('cart.show', ['id' => $cart->id])
+            ->with('error', 'Ups! Unknown error.');
     }
 }
