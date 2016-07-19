56ced265a6c84b4929a95e1660b0b5793feac329
ajpadilla <ajpadilla88@gmail.com>
2016-07-19 14:23:43 -0400

    Agregando parameto productId al metodo store del controlador

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -35,7 +35,7 @@ class WishlistController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $productId)
     {
         //
     }
