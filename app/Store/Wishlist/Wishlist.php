65727a1e5e8041918b57c0acff42dd463358b4f0
ajpadilla <ajpadilla88@gmail.com>
2016-07-19 13:53:05 -0400

    Agregando atibuto fillable a la clase

--- app/Store/Wishlist/Wishlist.php
+++ app/Store/Wishlist/Wishlist.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Store\Wishlist;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Wishlist extends Model
 {
-    //
+   protected $fillable = ['product_id', 'user_id'];
 }
