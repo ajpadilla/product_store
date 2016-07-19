b479e066bb243960042182709652d4e4e09835b8
ajpadilla <ajpadilla88@gmail.com>
2016-07-19 15:42:55 -0400

    Mostrando datos de productos agregados a lista de deseos

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -73,6 +73,30 @@
 						</span>
 					</a>
 					<ul id="products-wishlist" class="dropdown-menu dropdown-alerts">
+						@if (\Auth::user()->hasProductToWishlist())
+							@foreach (\Auth::user()->wishlist as $product)
+							<li class="li">
+								<div class="row">
+									<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
+										<a href="{{ route('product.show', $product->id) }}"> 
+											<i class="fa fa-check fa-2x"></i> {{ $product->name }}
+										</a>
+									</div>
+									<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+										{{ $product->pivot->quantity }}
+									</div>
+									<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+										<span class="pull-right text-muted small">
+											<a href="{{ route('cart.delete-ajax', $product->id) }}" 
+											class="delete-from-cart">
+												<i class="fa fa-minus-circle fa-2x"></i>
+											</a>
+										</span>
+									</div>
+								</div>
+							</li>
+							@endforeach
+						@endif
 						<li class="divider"></li>
 						<li>
 							<div class="text-center link-block">
