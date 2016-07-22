7021936a99666a950d6419a579b07a6413c9625c
ajpadilla <ajpadilla88@gmail.com>
2016-07-22 15:35:40 -0400

    Agregando ruta segura

--- resources/views/layouts/partials/_css.blade.php
+++ resources/views/layouts/partials/_css.blade.php
@@ -1,5 +1,5 @@
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/bootstrap-3.3.6-dist/css/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/bootstrap-3.3.6-dist/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
 <link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/custom/styles.css') }}">
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
