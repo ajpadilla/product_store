0154581c248933ff4955c7c07e51a4b2e9cf2a83
ajpadilla <ajpadilla88@gmail.com>
2016-07-21 21:36:47 -0400

    Actualizando blade

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -3,6 +3,10 @@
 @section('main-content')
 @include("layouts.partials._header")
 <div class="row">
+	<br/>
+	<br/>
+	<br/>
+	
 	@include('layouts.partials._errors')
 	@include('layouts.partials._messages')
 	<h1>Pagina Principal</h1>
