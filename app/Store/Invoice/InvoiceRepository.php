825eec5c4adb6d8c29807717ca9f3f541903363c
ajpadilla <ajpadilla88@gmail.com>
2016-08-01 19:22:22 -0400

    Actualizando metodo getDatatable

--- app/Store/Invoice/InvoiceRepository.php
+++ app/Store/Invoice/InvoiceRepository.php
@@ -27,10 +27,16 @@
 			$invoices = $this->getModel()->select(['id','date','client_id','address', 'total']);
 			return Datatables::of($invoices)
 			->addColumn('client_id', function($invoice){
-				return $invoice->client->name;
+				return $invoice->client->full_name;
 			})
 			->addColumn('address', function($invoice){
 				return $invoice->client->address;
+			})
+			->addColumn('action', function($invoice){
+				return "<a  class='btn btn-primary' href='" . route('invoice.create-pdf', $invoice->id) . "'>
+							<i class='glyphicon glyphicon-edit'></i>PDF
+						</a>
+				";
 			})
 			->make(true);
 		}
