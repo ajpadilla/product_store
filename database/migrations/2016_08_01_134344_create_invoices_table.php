a51f085b8eb1a2eef87298dafb19f4ce77c1969b
ajpadilla <ajpadilla88@gmail.com>
2016-08-01 09:49:35 -0400

    Agregando migración para facturas de compras realizadas por los usuarios

---
+++ database/migrations/2016_08_01_134344_create_invoices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->integer('user_id')->unsigned()->index();
+            $table->text('address');
+            $table->decimal('total',10,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('invoices');
+    }
+}
