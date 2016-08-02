f80e34ce3f8f88d7e74d650a3d8f9058f0b4eb20
ajpadilla <ajpadilla88@gmail.com>
2016-08-01 22:53:50 -0400

    Actualizando datos dinamicos a mostrar

--- resources/views/invoices/pdf/invoice.blade.php
+++ resources/views/invoices/pdf/invoice.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Invoice</title>
-    {!! Html::style('assets/css//invoice/style.css') !!}
+    {!! Html::style('assets/css/invoice/style.css') !!}
   </head>
   <body>
     <main>
@@ -11,32 +11,38 @@
         <div id="invoice">
           <h1>INVOICE {{ $invoice->id }}</h1>
           <div class="date">Date of Invoice: {{ $invoice->date }}</div>
+          <div class="date">Client: {{ $invoice->client->full_name }}</div>
+          <div class="date">Address: {{ $invoice->address }}</div>
         </div>
       </div>
       <table border="0" cellspacing="0" cellpadding="0">
         <thead>
           <tr>
             <th class="no">#</th>
-            <th class="desc">DESCRIPTION</th>
-            <th class="unit">UNIT PRICE</th>
+            <th class="desc">NAME</th>
+            <th class="unit">DESCRIPTION</th>
+            <th class="desc">QUANTITY</th>
+            <th class="unit">PRICE</th>
             <th class="total">TOTAL</th>
           </tr>
         </thead>
         <tbody>
-          @foreach ($invoice->orders as $order)
+          @foreach ($invoice->orders as $index => $order)
             <tr>
-              <td class="no">{{ $data['quantity'] }}</td>
-              <td class="desc">{{ $data['description'] }}</td>
-              <td class="unit">{{ $data['price'] }}</td>
-              <td class="total">{{ $data['total'] }} </td>
+              <td class="no">{{ $index + 1 }}</td>
+              <td class="desc">{{ $order->product->name }}</td>
+              <td class="unit">{{ $order->product->description }}</td>
+              <td class="desc">{{ $order->quantity }}</td>
+              <td class="unit">{{ $order->price }}</td>
+              <td class="total">{{ $order->amount  }} </td>
             </tr>
           @endforeach
         </tbody>
         <tfoot>
           <tr>
-            <td colspan="2"></td>
+            <td colspan="4"></td>
             <td >TOTAL</td>
-            <td>$6,500.00</td>
+            <td>{{ $invoice->total }}</td>
           </tr>
         </tfoot>
       </table>
