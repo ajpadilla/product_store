3527581496ed36b72ccc370e0b83dd9e671d632d
ajpadilla <ajpadilla88@gmail.com>
2016-08-03 14:24:14 -0400

    Agregando nuevo middleware

---
+++ app/Http/Middleware/EnabledUserAccount.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+
+class EnabledUserAccount
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        
+        return $next($request);
+    }
+}
