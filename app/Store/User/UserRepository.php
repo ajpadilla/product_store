babc442c2e4deee4950f10b27eeb77c037515183
ajpadilla <ajpadilla88@gmail.com>
2016-08-03 21:33:14 -0400

    Agregando metodo table, para listar usuarios existentes en el sistema

--- app/Store/User/UserRepository.php
+++ app/Store/User/UserRepository.php
@@ -3,6 +3,7 @@
 
 	use App\Store\Base\BaseRepository;
 	use App\Store\User\User;
+	use Yajra\Datatables\Datatables;
 
 	class UserRepository extends BaseRepository
 	{
@@ -33,13 +34,13 @@
 		public function table()
 		{
 			$users = $this->getModel()->select(['id','first_name','last_name','username', 'email', 'address','post_code', 'country_id', 'phone', 'active', 'role']);
-			return Datatables::of($products)
-			->addColumn('classification', function($users){
-				return $product->classification->name;
+			return Datatables::of($users)
+			->addColumn('country_id', function($user){
+				return $user->country->name;
 			})
-			->addColumn('photo', function($product){
+			->addColumn('photo', function($user){
 				$img = '';
-				$photo = $product->first_photo;
+				$photo = $user->first_photo;
 				if ($photo != false) {
 					$img .= "	<a href='#'>
 					<img class='mini-photo' alt='" . $photo->filename . "' src='" . asset($photo->complete_thumbnail_path) . "'>
@@ -47,19 +48,10 @@
 				}
 				return $img;
 			})
-			->addColumn('action', function($product){
-				return "<a  class='btn btn-primary' href='" . route('product.edit', $product->id) . "'>
+			->addColumn('action', function($user){
+				return "<a  class='btn btn-primary' href='" . route('product.edit', $user->id) . "'>
 							<i class='glyphicon glyphicon-edit'></i>Edit
 						</a><br/>
-						<a  class='btn btn-info show-product' href='#' id='show_product_".$product->id."'>
-							<i class='glyphicon glyphicon-zoom-in'></i>Show
-						</a><br/>
-						<a  class='btn btn-danger delete-products' href='' id='delete_products_".$product->id."'>
-							<i class='glyphicon glyphicon-remove'></i>Delete
-						</a><br/>
-						<a  class='btn btn-primary' href='" . route('photoProduct.create', $product->id) . "'>
-							<i class='glyphicon glyphicon-camera'></i>Add Photo
-						</a>
 				";
 			})
 			->make(true);
