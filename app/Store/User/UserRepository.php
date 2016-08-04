cf82aa0bca65e984b94377e6c4004dea856ba2a1
ajpadilla <ajpadilla88@gmail.com>
2016-08-03 21:10:32 -0400

    Agregando metodo getActiveAttribute

--- app/Store/User/UserRepository.php
+++ app/Store/User/UserRepository.php
@@ -30,6 +30,41 @@
 			return $user;
 		}
 
+		public function table()
+		{
+			$users = $this->getModel()->select(['id','first_name','last_name','username', 'email', 'address','post_code', 'country_id', 'phone', 'active', 'role']);
+			return Datatables::of($products)
+			->addColumn('classification', function($users){
+				return $product->classification->name;
+			})
+			->addColumn('photo', function($product){
+				$img = '';
+				$photo = $product->first_photo;
+				if ($photo != false) {
+					$img .= "	<a href='#'>
+					<img class='mini-photo' alt='" . $photo->filename . "' src='" . asset($photo->complete_thumbnail_path) . "'>
+					</a>";
+				}
+				return $img;
+			})
+			->addColumn('action', function($product){
+				return "<a  class='btn btn-primary' href='" . route('product.edit', $product->id) . "'>
+							<i class='glyphicon glyphicon-edit'></i>Edit
+						</a><br/>
+						<a  class='btn btn-info show-product' href='#' id='show_product_".$product->id."'>
+							<i class='glyphicon glyphicon-zoom-in'></i>Show
+						</a><br/>
+						<a  class='btn btn-danger delete-products' href='' id='delete_products_".$product->id."'>
+							<i class='glyphicon glyphicon-remove'></i>Delete
+						</a><br/>
+						<a  class='btn btn-primary' href='" . route('photoProduct.create', $product->id) . "'>
+							<i class='glyphicon glyphicon-camera'></i>Add Photo
+						</a>
+				";
+			})
+			->make(true);
+		}
+
 	}
 
 ?>
